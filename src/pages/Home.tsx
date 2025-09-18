08ec20a948c52ad987539e3ed5fb69ecd2c4d38b
fastoch <fastoch@ik.me>
2025-09-18 15:25:38 +0200

    starting home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+/**
+ * This is our Dashboard component
+ */
+
+
+const Home = () => {
+  return (
+    <div>
+      <SearchBar /> {/* Fabrice */}
+      <SearchResults /> {/* Fabrice */}
+      <BottomNav /> {/* Nordine */}
+    </div>
+  )
+}
+
+export default Home
